940b7dd6de5058512a9b74f541d73a16e9d6f600
James Wright <james@jamesswright.co.uk>
2015-10-21 13:35:19 +0100

    Convert spaces to tabs

--- JamesWright.PersonalityForge/PersonalityForgeDataService.cs
+++ JamesWright.PersonalityForge/PersonalityForgeDataService.cs
@@ -13,64 +13,64 @@ namespace JamesWright.PersonalityForge
 	{
 		private const string _host = "http://www.personalityforge.com/api/chat/";
 		private WebClient _client;
-        private IJsonHelper _jsonHelper;
-        private IUtils _utils;
+		private IJsonHelper _jsonHelper;
+		private IUtils _utils;
 
 		public PersonalityForgeDataService()
 		{
 			_client = new WebClient();
-            _jsonHelper = new JsonHelper();
-            _utils = new Utils();
+			_jsonHelper = new JsonHelper();
+			_utils = new Utils();
 		}
 
-        //constructor for dependency injection
-        public PersonalityForgeDataService(IJsonHelper jsonHelper, IUtils utils)
-        {
-            _client = new WebClient();
-            _jsonHelper = jsonHelper;
-            _utils = utils;
-        }
-
-        public Response Send(ApiInfo apiInfo, string username, string text)
+		//constructor for dependency injection
+		public PersonalityForgeDataService(IJsonHelper jsonHelper, IUtils utils)
 		{
-            Message message = CreateMessage(text, apiInfo.BotId);
-            User user = CreateUser(username);
+			_client = new WebClient();
+			_jsonHelper = jsonHelper;
+			_utils = utils;
+		}
 
-            Payload data = CreatePayload(message, user);
+		public Response Send(ApiInfo apiInfo, string username, string text)
+		{
+			Message message = CreateMessage(text, apiInfo.BotId);
+			User user = CreateUser(username);
+
+			Payload data = CreatePayload(message, user);
 
 			string dataJson = _jsonHelper.ToJson<Payload>(data);
-            string request = GetRequestUri(apiInfo, dataJson);
+			string request = GetRequestUri(apiInfo, dataJson);
 
 			try 
 			{
-                return _jsonHelper.ToObject<Response>(MakeRequest(request));
+				return _jsonHelper.ToObject<Response>(MakeRequest(request));
 			}
 			catch (Exception e)
 			{
-                throw new PersonalityForgeException(e.Message, e);
+				throw new PersonalityForgeException(e.Message, e);
 			}
 		}
 
-        public async Task<Response> SendAsync(ApiInfo apiInfo, string username, string text)
-        {
-            Message message = CreateMessage(text, apiInfo.BotId);
-            User user = CreateUser(username);
+		public async Task<Response> SendAsync(ApiInfo apiInfo, string username, string text)
+		{
+			Message message = CreateMessage(text, apiInfo.BotId);
+			User user = CreateUser(username);
 
-            Payload data = CreatePayload(message, user);
+			Payload data = CreatePayload(message, user);
 
-            string dataJson = _jsonHelper.ToJson<Payload>(data);
-            string request = GetRequestUri(apiInfo, dataJson);
+			string dataJson = _jsonHelper.ToJson<Payload>(data);
+			string request = GetRequestUri(apiInfo, dataJson);
 
-            try
-            {
-                string responseJson = await MakeRequestAsync(request);
-                return _jsonHelper.ToObject<Response>(responseJson);
-            }
-            catch (Exception e)
-            {
-                throw new PersonalityForgeException(e.Message, e);
-            }
-        }
+			try
+			{
+				string responseJson = await MakeRequestAsync(request);
+				return _jsonHelper.ToObject<Response>(responseJson);
+			}
+			catch (Exception e)
+			{
+				throw new PersonalityForgeException(e.Message, e);
+			}
+		}
 
 		private string MakeRequest(string request)
 		{
@@ -82,66 +82,66 @@ namespace JamesWright.PersonalityForge
 			} 
 			catch (WebException e) 
 			{
-                throw new PersonalityForgeException(e.Message, e);
+				throw new PersonalityForgeException(e.Message, e);
 			} 
 			catch (Exception e) 
 			{
-                throw new PersonalityForgeException(e.Message, e);
+				throw new PersonalityForgeException(e.Message, e);
 			}
 
-            return response != null ? _utils.FilterJson(response) : null;
+			return response != null ? _utils.FilterJson(response) : null;
 		}
 
-        private async Task<string> MakeRequestAsync(string request)
-        {
-            byte[] response = null;
+		private async Task<string> MakeRequestAsync(string request)
+		{
+			byte[] response = null;
 
-            try
-            {
-                response = await _client.DownloadDataTaskAsync(new Uri(request));
-            }
-            catch (WebException e)
-            {
-                throw new PersonalityForgeException(e.Message, e);
-            }
-            catch (Exception e)
-            {
-                throw new PersonalityForgeException(e.Message, e);
-            }
+			try
+			{
+				response = await _client.DownloadDataTaskAsync(new Uri(request));
+			}
+			catch (WebException e)
+			{
+				throw new PersonalityForgeException(e.Message, e);
+			}
+			catch (Exception e)
+			{
+				throw new PersonalityForgeException(e.Message, e);
+			}
 
-            return response != null ? _utils.FilterJson(response) : null;
-        }
+			return response != null ? _utils.FilterJson(response) : null;
+		}
 
-        private User CreateUser(string username)
-        {
-            return new User
-            {
-                ExternalID = username
-            };
-        }
+		private User CreateUser(string username)
+		{
+			return new User
+			{
+				ExternalID = username
+			};
+		}
 
-        private Message CreateMessage(string text, int botId)
-        {
-            return new Message
-            {
-                Text = text,
-                Timestamp = _utils.GenerateTimestamp(),
-                ChatBotId = botId
-            };
-        }
+		private Message CreateMessage(string text, int botId)
+		{
+			return new Message
+			{
+				Text = text,
+				Timestamp = _utils.GenerateTimestamp(),
+				ChatBotId = botId
+			};
+		}
 
-        private Payload CreatePayload(Message message, User user)
-        {
-            return new Payload
-            {
-                Message = message,
-                User = user
-            };
-        }
+		private Payload CreatePayload(Message message, User user)
+		{
+			return new Payload
+			{
+				Message = message,
+				User = user
+			};
+		}
 
-        private string GetRequestUri(ApiInfo apiInfo, string dataJson)
-        {
-            return string.Format("{0}?apiKey={1}&hash={2}&message={3}", _host, apiInfo.Key, _utils.GenerateSecret(apiInfo.Secret, dataJson), HttpUtility.UrlEncode(dataJson));
-        }
+		private string GetRequestUri(ApiInfo apiInfo, string dataJson)
+		{
+			return string.Format("{0}?apiKey={1}&hash={2}&message={3}", _host, apiInfo.Key, _utils.GenerateSecret(apiInfo.Secret, dataJson), HttpUtility.UrlEncode(dataJson));
+		}
 	}
 }
