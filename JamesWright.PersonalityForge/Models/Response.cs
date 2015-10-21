4bad2eae331bc98699faac3a38432a02d7304f25
James Wright <james@jamesswright.co.uk>
2015-10-21 13:38:57 +0100

    Convert spaces to tabs

--- JamesWright.PersonalityForge/Models/Response.cs
+++ JamesWright.PersonalityForge/Models/Response.cs
@@ -6,14 +6,13 @@ namespace JamesWright.PersonalityForge.Models
 	[DataContract]
 	public class Response
 	{
-        [DataMember(Name = "message")]
-        public Message Message { get; set; }
+		[DataMember(Name = "message")]
+		public Message Message { get; set; }
 
-        [DataMember(Name = "success")]
-        public int Success { get; set; }
+		[DataMember(Name = "success")]
+		public int Success { get; set; }
 
-        [DataMember(Name = "errorMessage")]
-        public string ErrorMessage { get; set; }
+		[DataMember(Name = "errorMessage")]
+		public string ErrorMessage { get; set; }
 	}
 }
-
